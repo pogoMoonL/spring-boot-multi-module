ec91f34ee28d804f4655d48508b14c57b7d6f1d1
Nick Huang 黃耀德 <nickhuang@hitrust.com.tw>
2021-05-29 19:33:40 +0800

    fix: test

---
+++ controller/src/main/java/com/purnima/jain/customer/repository/NoteRepository.java
@@ -0,0 +1,15 @@
+package com.purnima.jain.customer.repository;
+
+import com.purnima.jain.customer.model.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by rajeevkumarsingh on 27/06/17.
+ */
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, Long> {
+
+}
+
--- domain/src/main/java/com/purnima/jain/customer/domain/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.purnima.jain.customer.domain.repository;
-
-import com.purnima.jain.customer.domain.aggregate.Customer;
-
-public interface CustomerRepository {
-
-	public Customer getCustomerById(Integer customerId);
-
-}
